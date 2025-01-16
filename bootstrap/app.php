1710de469f3676a2e40a62a225bfbf37c4f0ccab
Shift <shift@laravelshift.com>
2025-01-16 08:43:20 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,37 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo('/');
+
+        $middleware->validateCsrfTokens(except: [
+            'xmlfeed',
+        ]);
+
+        $middleware->append(\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class);
+
+        $middleware->web(\App\Http\Middleware\LocaleMiddleware::class);
+
+        $middleware->throttleApi('60,1');
+
+        $middleware->group('admin', [
+            'auth',
+            'access.routeNeedsPermission:view-backend',
+            'timeout',
+        ]);
+
+        $middleware->alias([
+            'CheckUserStatus' => \App\Http\Middleware\CheckUserStatus::class,
+            'OnlyUsers' => \App\Http\Middleware\OnlyUsers::class,
+            'access.routeNeedsPermission' => \App\Http\Middleware\RouteNeedsPermission::class,
+            'access.routeNeedsRole' => \App\Http\Middleware\RouteNeedsRole::class,
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'checkDeletedUserOffer' => \App\Http\Middleware\CheckDeletedUserHasOffer::class,
+            'checkOfferValues' => \App\Http\Middleware\CheckSessionHasOfferValues::class,
+            'checkPropertyId' => \App\Http\Middleware\CheckSessionHasOfferPropertyId::class,
+            'checkSignatureValues' => \App\Http\Middleware\CheckSessionHasOfferSignature::class,
+            'timeout' => \App\Http\Middleware\SessionTimeout::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
